1d02a2e1d253abbbbd0cd91b470a5ae39c41cacb
wavy <jflortech@gmail.com>
2024-10-05 19:16:00 +0000

    Finished look and added forum basic logic

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -7,6 +7,7 @@ import AboutUs from "./components/AboutUs";
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/registerPage";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PostDetails from "@/components/PostDetails.tsx";
 
 const App = () => {
     return (
@@ -17,6 +18,7 @@ const App = () => {
                 <Route path="/" element={<MainPage />} />
                 <Route path="/Exoplanets" element={<Exoplanets/>}/>
                 <Route path="/Forum" element={<Forum/>}/>
+                <Route path="/Forum/Posts/:postId" element={<PostDetails />} />
                 <Route path="/AboutUs" element={<AboutUs/>}/>
                 <Route path="/loginPage" element={<LoginPage/>}/>
                 <Route path="/registerPage" element={<RegisterPage/>} />
