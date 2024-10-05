861a2b4b4d43e8d021a6715053564c6a49a1edcf
Andrz3jj <andrzej1332@onet.pl>
2024-10-05 17:29:33 +0200

    change contact page

--- landing-page/src/components/Contact.tsx
+++ landing-page/src/components/Contact.tsx
@@ -8,17 +8,20 @@ const Contact = () => {
           <div className="bg-slate-700 border border-slate-600 rounded p-8 text-center max-w-md mx-auto">
             <span className="text-3xl">Skontaktuj się z nami</span><br/><br/>
             <span className="flex">Imię i nazwisko</span>
-            <input type="text" 
-              className="bg-slate-600 text-lg p-2 rounded shadow border border-slate-600 w-full focus:outline-none focus:shadow-lg transition"/>
-            <span className="flex mt-4">E-mail</span>
-            <input type="text" 
-              className="bg-slate-600 text-lg p-2 rounded shadow border border-slate-600 w-full focus:outline-none focus:shadow-lg transition"/>
-            <span className="flex mt-4">Telefon kontaktowy</span>
-            <input type="text" 
-              className="bg-slate-600 text-lg p-2 rounded shadow border border-slate-600 w-full focus:outline-none focus:shadow-lg transition"/>
-            <span className="flex mt-4">Wiadomość</span>
-            <textarea 
-              className="bg-slate-600 text-lg p-2 rounded shadow border border-slate-600 w-full focus:outline-none focus:shadow-lg transition"></textarea>
+            <form>
+                <input type="text" 
+                className="bg-slate-600 text-lg p-2 rounded shadow border border-slate-600 w-full focus:outline-none focus:shadow-lg transition"/>
+                <span className="flex mt-4">E-mail</span>
+                <input type="text" 
+                className="bg-slate-600 text-lg p-2 rounded shadow border border-slate-600 w-full focus:outline-none focus:shadow-lg transition"/>
+                <span className="flex mt-4">Telefon kontaktowy</span>
+                <input type="text" 
+                className="bg-slate-600 text-lg p-2 rounded shadow border border-slate-600 w-full focus:outline-none focus:shadow-lg transition"/>
+                <span className="flex mt-4">Wiadomość</span>
+                <textarea 
+                className="bg-slate-600 text-lg p-2 rounded shadow border border-slate-600 w-full focus:outline-none focus:shadow-lg transition"></textarea>
+                <input type="submit" value="Prześlij zgłoszenie" className="mt-4 bg-blue-600 rounded p-4 w-full hover:bg-blue-700 transition focus:bg-blue-800"/>
+                </form>
           </div>
         </div>
       </div>
