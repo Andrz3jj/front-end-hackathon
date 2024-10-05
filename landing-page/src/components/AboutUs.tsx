373cb7425fadf4bf45a8291d178a60b8efc85840
Andrz3jj <andrzej1332@onet.pl>
2024-10-05 13:34:19 +0200

    add responsive to aboutUs

--- landing-page/src/components/AboutUs.tsx
+++ landing-page/src/components/AboutUs.tsx
@@ -10,7 +10,7 @@ const AboutUs = () => {
     return (
         <>
             <div className="bg-slate-800 text-white min-h-screen">
-               <div className="flex items-center justify-center h-full p-16 container">
+               <div className="flex items-center justify-center h-full p-4 md:p-16 container">
                     <div className="text-lg text-center">
                         <div>
                             <span className="font-bold">Prawy Przycisk Myszy</span> to zespół kreatywnych i pasjonatów technologii programistów, 
@@ -20,14 +20,14 @@ const AboutUs = () => {
                             planet znajdujących się poza Układem Słonecznym, 
                             w sposób przystępny zarówno dla laików, jak i dla pasjonatów astronomii.
                         </div>
-                        <div className="text-lg text-center pt-16">
+                        <div className="text-lg text-center pt-8 md:pt-16">
                             Nasz zespół składa się z programistów z różnych dziedzin, w tym:<br/><br/>
                             <ul>
                                 <li>Front-end developers:<br/><br/>
-                                <div className="grid grid-cols-3 gap-4">
+                                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
 
                                     <div className="bg-slate-700 border border-slate-600 rounded flex items-center">
-                                        <img src={Andrzej} className="w-28 m-4 rounded-lg" />
+                                        <img src={Andrzej} className="w-20 md:w-28 m-4 rounded-lg" />
                                         <div className="ml-4 text-white flex flex-col items-center"> 
                                              <span className="text-center">Andrzej Bajcarczyk</span> 
                                             <a href="https://github.com/Andrz3jj" 
@@ -39,7 +39,7 @@ const AboutUs = () => {
                                     </div>
 
                                     <div className="bg-slate-700 border border-slate-600 rounded flex items-center"> 
-                                        <img src={Janek} className="w-28 m-4 rounded-lg" />
+                                        <img src={Janek} className="w-20 md:w-28 m-4 rounded-lg" />
                                         <div className="ml-4 text-white flex flex-col items-center"> 
                                             <span className="text-center">Jan Florek</span> 
                                             <a href="https://github.com/WavyWare" 
@@ -51,7 +51,7 @@ const AboutUs = () => {
                                     </div>
 
                                     <div className="bg-slate-700 border border-slate-600 rounded flex items-center">
-                                        <img src={Tomek} className="w-28 m-4 rounded-lg" />
+                                        <img src={Tomek} className="w-20 md:w-28 m-4 rounded-lg" />
                                         <div className="ml-4 text-white flex flex-col items-center"> 
                                             <span className="text-center">Tomasz Tłusty</span> 
                                             <a href="https://github.com/FarciarzYT" 
@@ -66,10 +66,10 @@ const AboutUs = () => {
 
                                 </li><br/>
                                 <li>Back-end developers:</li><br/>
-                                <div className="grid grid-cols-2 gap-4">
+                                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
 
                                     <div className="bg-slate-700 border border-slate-600 rounded flex items-center">
-                                        <img src={Kamil} className="w-28 m-4 rounded-lg" />
+                                        <img src={Kamil} className="w-20 md:w-28 m-4 rounded-lg" />
                                               <div className="ml-4 text-white flex flex-col items-center">
                                                 <span className="text-center">Kamil Konieczny</span>
                                                 <a href="https://github.com/kamelkali" 
@@ -80,7 +80,7 @@ const AboutUs = () => {
                                             </div>
                                     </div>
                                     <div className="bg-slate-700 border border-slate-600 rounded flex items-center">
-                                        <img src={Kacper} className="w-28 m-4 rounded-lg" />
+                                        <img src={Kacper} className="w-20 md:w-28 m-4 rounded-lg" />
                                             <div className="ml-4 text-white flex flex-col items-center">
                                                 <span className="text-center">Kacper Zbyrad</span>
                                                 <a href="https://github.com/KacZbyDev" 
