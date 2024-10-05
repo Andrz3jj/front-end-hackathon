9dc2d314bafc83eed7dc3bb3bda9e9c75ea89c6e
wavy <jflortech@gmail.com>
2024-10-05 12:36:43 +0000

    Imported Bootstrap Icons

--- landing-page/src/main.tsx
+++ landing-page/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
