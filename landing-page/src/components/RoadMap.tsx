425afe0a149ab6c362835aa14238b79cab1bd961
Andrz3jj <andrzej1332@onet.pl>
2024-10-06 07:36:01 +0200

    add new text to roadmap

--- landing-page/src/components/RoadMap.tsx
+++ landing-page/src/components/RoadMap.tsx
@@ -5,11 +5,10 @@ const Roadmap = () => {
     <div className="bg-slate-800 p-4">
       <div className="flex flex-col grid-cols-9 p-2 mx-auto md:grid">
         {/* Event 1 */}
-        <div className="flex md:contents flex-row-reverse">
+        <div className="flex md:contents flex-row-reverse text-center">
           <div className="relative p-4 my-6 border border-slate-600 bg-slate-700 text-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
-            <h3 className="text-lg font-semibold lg:text-xl">New Event 1</h3>
-            <p className="mt-2 leading-6">Description of the first event.</p>
-            <span className="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 1</span>
+            <h3 className="text-lg font-semibold lg:text-xl">Biblioteka egzoplanet</h3>
+            <p className="mt-2 leading-6">Przeglądaj zaktualizowaną bazę danych egzoplanet z całego wszechświata.</p>
           </div>
           <div className="relative col-start-5 col-end-6 mr-7 md:mx-auto">
             <div className="flex items-center justify-center w-6 h-full">
@@ -20,7 +19,7 @@ const Roadmap = () => {
         </div>
 
         {/* Event 2 */}
-        <div className="flex md:contents">
+        <div className="flex md:contents text-center">
           <div className="relative col-start-5 col-end-6 mr-7 md:mx-auto">
             <div className="flex items-center justify-center w-6 h-full">
               <div className="w-1 h-full bg-indigo-300"></div>
@@ -28,18 +27,16 @@ const Roadmap = () => {
             <div className="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
           </div>
           <div className="relative p-4 my-6 bg-slate-700 border border-slate-600 text-white rounded-xl col-start-6 col-end-10 mr-auto">
-            <h3 className="text-lg font-semibold lg:text-xl">New Event 2</h3>
-            <p className="mt-2 leading-6">Description of the second event.</p>
-            <span className="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 2</span>
+            <h3 className="text-lg font-semibold lg:text-xl">Zostań odkrywcą</h3>
+            <p className="mt-2 leading-6">Specjalna sekcja z interaktywnymi quizami, które pomogą Ci nauczyć się, jak odkrywa się egzoplanety.</p>
           </div>
         </div>
 
         {/* Event 3 */}
-        <div className="flex md:contents flex-row-reverse">
+        <div className="flex md:contents flex-row-reverse text-center">
           <div className="relative p-4 my-6 bg-slate-700 border border-slate-600 text-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
-            <h3 className="text-lg font-semibold lg:text-xl">New Event 3</h3>
-            <p className="mt-2 leading-6">Description of the first event.</p>
-            <span className="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 3</span>
+            <h3 className="text-lg font-semibold lg:text-xl">Profile planet</h3>
+            <p className="mt-2 leading-6">Szczegółowe informacje o każdej planecie: odległość, masa, typ gwiazdy macierzystej i więcej.</p>
           </div>
           <div className="relative col-start-5 col-end-6 mr-7 md:mx-auto">
             <div className="flex items-center justify-center w-6 h-full">
@@ -50,7 +47,7 @@ const Roadmap = () => {
         </div>
 
         {/* Event 4 */}
-        <div className="flex md:contents">
+        <div className="flex md:contents text-center">
           <div className="relative col-start-5 col-end-6 mr-7 md:mx-auto">
             <div className="flex items-center justify-center w-6 h-full">
               <div className="w-1 h-full bg-indigo-300"></div>
@@ -58,9 +55,8 @@ const Roadmap = () => {
             <div className="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
           </div>
           <div className="relative p-4 my-6 bg-slate-700 border border-slate-600 text-white rounded-xl col-start-6 col-end-10 mr-auto">
-            <h3 className="text-lg font-semibold lg:text-xl">New Event 4</h3>
-            <p className="mt-2 leading-6">Description of the second event.</p>
-            <span className="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 4</span>
+            <h3 className="text-lg font-semibold lg:text-xl">Tryb nauki</h3>
+            <p className="mt-2 leading-6">Moduł, który pomoże użytkownikom zrozumieć proces odkrywania egzoplanet i różne metody badawcze.</p>
           </div>
         </div>
       </div>
